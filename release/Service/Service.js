fcc3bfdc6709e5e5a37c02f5fb577f19a0d4c755
Kaibito <gpuzzletime@icloud.com>
2017-03-15 01:28:17 +0300

    Removed release folder

--- release/Service/Service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Service = (function () {
-    function Service() {
-    }
-    return Service;
-}());
-exports.Service = Service;
-//# sourceMappingURL=Service.js.map
