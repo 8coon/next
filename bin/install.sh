a302b23c33326da70b51839a06ba01d438b9d802
vileven <vileven11@icloud.com>
2017-04-03 16:34:51 +0300

    Changed intall.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,6 @@
 
 node ./../tslint/bin/tslint './src/**/*.ts?(x)' --type-check --project ./tsconfig.json &&
 
-node ./..//typescript/bin/tsc &&
+#node ./..//typescript/bin/tsc &&
 echo "JSWorks={};__JSWorks_services__=[];function a(r){r.keys().forEach(r);};a(require.context('./',true,/\.js$/));CSSauron = require('../cssauron/index.js');" > ./release/entry.js &&
 node ./..//webpack/bin/webpack.js
